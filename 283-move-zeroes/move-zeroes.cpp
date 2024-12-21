ab62e9d28699abac35157451452d7baf9dba1e59
AyushAntil007 <ayushantil2023@gmail.com>
2024-12-21 11:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (20.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,19 +5,32 @@ public:
 
         //brute force
 
-       vector<int>temp;
-       for(int i=0;i<n;i++){
-        if(nums[i]!=0){
-            temp.push_back(nums[i]);
+    //    vector<int>temp;
+    //    for(int i=0;i<n;i++){         //store non zero elt
+    //     if(nums[i]!=0){
+    //         temp.push_back(nums[i]);
+    //     }
+    //    }
+    //    for(int i=0;i<temp.size();i++){  //place non zero elt in front
+    //         nums[i]=temp[i];
+    //    }
+    //    for(int i=temp.size();i<n;i++){   //fill zero at end
+    //     nums[i]=0;
+    //    }
+
+
+    //optimal solution
+
+    int index=0;
+    for(int j=0;j<n;j++){     //two pointer approach
+        if(nums[j]!=0){
+            nums[index]=nums[j];
+            index++;
         }
-       }
-       for(int i=0;i<temp.size();i++){
-            nums[i]=temp[i];
-       }
-       for(int i=temp.size();i<n;i++){
+    }
+    for(int i=index;i<n;i++){
         nums[i]=0;
-       }
-       
+    }
 
     }
 };
