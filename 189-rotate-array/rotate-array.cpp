e1842d90e1ae4311583f5366390f2118f879a667
AyushAntil007 <ayushantil2023@gmail.com>
2024-12-21 10:04:38 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (41.61%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
+    void reverse(vector<int>&nums, int start, int end) {
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n; // to ignore the full array rotation
+
+        if (n == 0 || k == 0)
+            return;
 
         // brute force
 
@@ -16,21 +28,20 @@ public:
 
         // better solution
 
-        if (n == 0||k==0)
-            return;
-        k = k % n;
-       
-        int temp[k];
-        for (int i = n - k; i < n; i++) {
-            temp[i - n + k] = nums[i]; // copy rotated elts
-        }
-        for (int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i]; // shifting
-        }
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i]; // paste elts
-        }
+        // int temp[k];
+        // for (int i = n - k; i < n; i++) {
+        //     temp[i - n + k] = nums[i]; // copy rotated elts
+        // }
+        // for (int i = n - k - 1; i >= 0; i--) {
+        //     nums[i + k] = nums[i]; // shifting
+        // }
+        // for (int i = 0; i < k; i++) {
+        //     nums[i] = temp[i]; // paste elts
+        // }
 
         // optimal solution
+        reverse(nums, 0, n - k-1);
+        reverse(nums, n - k , n-1);
+        reverse(nums, 0, n-1);
     }
 };
