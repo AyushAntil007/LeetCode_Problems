ce5275b3fb0e89073b0ec6784eea2178f2e3edfa
AyushAntil007 <ayushantil2023@gmail.com>
2025-01-20 08:57:35 +0530

    Time: 1 ms (18.03%) | Memory: 17.7 MB (51.09%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,15 +5,30 @@ public:
 
         //brute force
 
-        int largeProd=nums[0];
-        for(int i=0;i<n;i++){
-            int prod=1;
-            for(int j=i;j<n;j++){
-                 prod=prod*nums[j];
-                 largeProd=max(largeProd,prod);
-            }
+        // int largeProd=nums[0];
+        // for(int i=0;i<n;i++){
+        //     int prod=1;
+        //     for(int j=i;j<n;j++){
+        //          prod=prod*nums[j];
+        //          largeProd=max(largeProd,prod);
+        //     }
             
+        // }
+        // return largeProd;
+
+        //optimal sol........kadanes algo
+
+        int posprod=nums[0];
+        int negprod=nums[0];
+        int tprod=nums[0];  //***important to initialise with nums[0]
+
+        for(int i=1;i<n;i++){
+           int temp=max(nums[i],max(posprod*nums[i],negprod*nums[i]));   //keep positive product
+            negprod=min(nums[i],min(posprod*nums[i],negprod*nums[i]));  //to store previous negative product arary
+            posprod=temp;
+            tprod=max(posprod,tprod);
         }
-        return largeProd;
+         return tprod;
+
     }
 };
