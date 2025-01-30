321539672c4a1175f5d1199321ade88f09082bac
AyushAntil007 <ayushantil2023@gmail.com>
2025-01-30 08:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (71.50%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,35 +2,53 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n = s.length();
-        string ans = "";
-
-        int start = 0;
-        int end = 0;
-        int i = 0;
-
+       string ans="";
         stack<string> st;
 
-        while (i < n) {
-            while (i < n && s[i] == ' ') {
-                i++;
-            }
-            start = i;
-            while (i < n && s[i] != ' ') {
-                i++;
+        // my approach....using set
+        //  int start = 0;
+        //  int end = 0;
+        //  int i = 0;
+
+        // while (i < n) {
+        //     while (i < n && s[i] == ' ') {
+        //         i++;
+        //     }
+        //     start = i;
+        //     while (i < n && s[i] != ' ') {
+        //         i++;
+        //     }
+        //     end = i;
+        //     if (start < end) { // Ensure a word was found
+        //         st.push(s.substr(start, end - start));
+        //     }
+        // }
+
+        //  while (!st.empty()) {
+        //     ans += st.top();
+        //     st.pop();
+        //     if (!st.empty()) ans += " "; // Add space between words
+        // }
+
+//optimal sol.....traverse from back
+         int start = n-1;
+         int end = n-1;
+         int i = n-1;
+
+        while (i >=0) {
+            while (i >=0 && s[i] == ' ') {
+                i--;
             }
             end = i;
+            while (i >=0 && s[i] != ' ') {
+                i--;
+            }
+            start = i;
             if (start < end) { // Ensure a word was found
-                st.push(s.substr(start, i - start));
+               ans+=s.substr(start+1,end-start)+' ';
             }
         }
-
-        while (!st.empty()) {
-            ans += st.top();
-            st.pop();
-            if (!st.empty())
-                ans += " "; // Add space between words
-        }
-
+        if (!ans.empty()) ans.pop_back(); // Remove trailing space
         return ans;
     }
 };
