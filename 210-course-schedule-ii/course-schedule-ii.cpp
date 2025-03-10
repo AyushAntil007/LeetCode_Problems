f0d0299584d9c3b0d21987dc1e91be0edc10975b
AyushAntil007 <ayushantil2023@gmail.com>
2025-03-10 20:27:51 +0530

    Time: 4 ms (60.08%) | Memory: 18.6 MB (49.72%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+         int n=prerequisites.size();
+        int m=numCourses;
+        vector<vector<int>>adj(m);  //dont forget to initialise
+
+        for(int i=0;i<n;i++){
+            vector<int>vec=prerequisites[i];
+            int u=vec[0];
+            int v=vec[1];
+            adj[v].push_back(u);
+        }
+      
+        vector<int>indeg(m,0);
+
+        for(int i=0;i<m;i++){
+            for(auto it:adj[i]){
+                indeg[it]++;
+            }
+        }
+        queue<int>q;
+
+        for(int i=0;i<m;i++){
+            if(indeg[i]==0)q.push(i);
+        }
+        int count=0;
+        vector<int>ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            count++;
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)q.push(it);
+            }
+        }
+        if(count==m)return ans;   //to find that all courses are done after the rquired ones...just detct cycle
+        return {};
+    }
+};
