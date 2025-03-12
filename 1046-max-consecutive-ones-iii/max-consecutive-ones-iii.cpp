5762a4c6d58471d2f271df50190d2db6136f3ada
AyushAntil007 <ayushantil2023@gmail.com>
2025-03-12 10:53:37 +0530

    Time: 0 ms (100.00%) | Memory: 59.2 MB (59.32%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -5,22 +5,27 @@ public:
 
         int maxCount=0;
         int count0=0;
-        vector<int>idx;
+        // vector<int>idx;
 
         int l=0,r=0;
         while(r<n){
             if(nums[r]==0){
                 count0++;
-                idx.push_back(r);
+                //idx.push_back(r);
             }
             if(count0>k){
-                count0--;
-                l=idx[0]+1;
-                idx.erase(idx.begin());
+                // count0--;
+                // l=idx[0]+1;
+                // idx.erase(idx.begin());
+
+                if(nums[l]==0)count0--;
+                l++;
             }
-           
-            maxCount=max(maxCount,r-l+1);
-            r++;  
+            if(count0<=k){
+                maxCount=max(maxCount,r-l+1);
+            }
+            r++; 
+             
         }
 
         return maxCount;
