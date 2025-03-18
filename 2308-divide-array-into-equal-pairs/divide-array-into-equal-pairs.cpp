f0b71012824e91f0d1726cbb95684da5802076ee
AyushAntil007 <ayushantil2023@gmail.com>
2025-03-18 05:33:35 +0530

    Time: 4 ms (42.52%) | Memory: 18 MB (48.93%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -1,41 +1,35 @@
-// class Solution {
-// public:
-//     bool divideArray(vector<int>& nums) {
-//         int n=nums.size();
-//         unordered_map<int,int>freq;
-
-//         for(int i=0;i<n;i++){
-//             freq[nums[i]]++;
-
-//         }
-//         int count=0;
-//         for(auto it: freq){
-//             if(it.second % 2 != 0) return false; 
-//             while((it.second)%2==0){
-//                 it.second=it.second/2;
-//                 count++;
-//             }
-        
-//         }
-//         if(count==n/2)return true;
-//         return false;
-    
-//     }
-// }; 
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int, int> counter;
-        for (int num : nums) {
-            counter[num]++;
+        int n=nums.size();
+        unordered_map<int,int>freq;
+
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+
         }
-        
-        for (const auto& [_, count] : counter) {
-            if (count % 2 != 0) {
-                return false;
-            }
+        int count=0;
+        for(auto it: freq){
+            if(it.second % 2 != 0) return false; 
         }
-        
         return true;
+   
     }
 }; 
+// class Solution {
+// public:
+//     bool divideArray(vector<int>& nums) {
+//         unordered_map<int, int> counter;
+//         for (int num : nums) {
+//             counter[num]++;
+//         }
+        
+//         for (const auto& [_, count] : counter) {
+//             if (count % 2 != 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }; 
