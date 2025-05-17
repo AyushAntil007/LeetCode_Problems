08fdd86cbb7e243982d0fa24769d5af08db04f51
AyushAntil007 <ayushantil2023@gmail.com>
2025-05-17 14:20:29 +0530

    Time: 2 ms (90.79%) | Memory: 15.3 MB (51.11%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -24,13 +24,39 @@ int countSum(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
             tsum+=nums[i];
         }
 
-        if(tsum<target)return 0;
-        if((tsum-target)%2==1)return 0;
+        if(tsum<(target))return 0;     //take absolute target
+        if((tsum-target)%2!=0)return 0;
 
-        int s1=(tsum-target)/2;   //new target
+        int s1=(tsum-target)/2;   //new target....minimum
 
-        vector<vector<int>>dp(n,vector<int>(s1+1,-1));
+       
     
-        return countSum(n-1,s1,nums,dp);
+       //memoisation....
+       //   vector<vector<int>>dp(n,vector<int>(s1+1,-1));
+
+       // return countSum(n-1,s1,nums,dp);
+
+
+       //tabulation......
+
+        vector<vector<int>>dp(n,vector<int>(s1+1,0));  //declare by 0 noy by -1
+        // Base cases
+        if (nums[0] == 0) dp[0][0] = 2;  // pick or not pick
+        else dp[0][0] = 1;              // only one way: not pick
+
+        if (nums[0] != 0 && nums[0] <= s1)
+            dp[0][nums[0]] = 1;
+
+       for(int i=1;i<n;i++){
+        for(int j=0;j<s1+1;j++){
+            int not_take=dp[i-1][j];
+            int take=0;
+            if(nums[i]<=j){
+                take=dp[i-1][j-nums[i]];
+            }
+            dp[i][j]=not_take+take;
+        }
+       }
+       return dp[n-1][s1];
     }
 };
