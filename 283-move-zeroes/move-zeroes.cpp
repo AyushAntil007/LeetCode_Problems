4a26eee24ab72f271f4a84c5e3274598f80e03e1
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-07 13:38:18 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (18.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,35 +2,12 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-
-        //brute force
-
-    //    vector<int>temp;
-    //    for(int i=0;i<n;i++){         //store non zero elt
-    //     if(nums[i]!=0){
-    //         temp.push_back(nums[i]);
-    //     }
-    //    }
-    //    for(int i=0;i<temp.size();i++){  //place non zero elt in front
-    //         nums[i]=temp[i];
-    //    }
-    //    for(int i=temp.size();i<n;i++){   //fill zero at end
-    //     nums[i]=0;
-    //    }
-
-
-    //optimal solution
-
-    int index=0;
-    for(int j=0;j<n;j++){     //two pointer approach
-        if(nums[j]!=0){
-            nums[index]=nums[j];
-            index++;
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
-    }
-    for(int i=index;i<n;i++){
-        nums[i]=0;
-    }
-
     }
 };
