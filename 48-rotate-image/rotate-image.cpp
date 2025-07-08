cf8ea1d55c9bb1d481edf1239dd199e5e6b5819f
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-08 14:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.27%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& a) {
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int m=matrix.size();
+        int n=matrix[0].size();
 
-        // brute force
-        //  int row=arr1.size();
-        //  int col=arr1[0].size();
-        //   vector<vector<int>> arr2(col, vector<int>(row));
-        //  for(int r=row-1;r>=0;r--){
-        //      for(int c=0;c<col;c++){
-        //           arr2[c][row-1-r]=arr1[r][c];
-        //      }
-        //  }
-        //  arr1=arr2;
-
-        // optimal sol
-
-        int m = a.size();
-        int n = a[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < i; j++) {
-                swap(a[i][j], a[j][i]); // taking transpose
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i<=j){
+                    swap(matrix[i][j],matrix[j][i]);
+                }
             }
         }
 
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n / 2; j++) {
-        //         swap(a[i][j], a[i][n - 1 - j]); // reverse each rows
-        //     }
-        // }
-
-        // reversing each row of the matrix
-        for (int i = 0; i < m; i++) {
-            reverse(a[i].begin(), a[i].end());
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            for(int i=0;i<m;i++){
+                swap(matrix[i][left],matrix[i][right]);
+                
+            }
+            left++;
+            right--;
         }
     }
 };
