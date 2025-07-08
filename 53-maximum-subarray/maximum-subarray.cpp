c5d7587666c924de42fab96b85d55545175fe5c6
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-08 18:16:44 +0530

    Time: 2 ms (22.67%) | Memory: 71.5 MB (99.96%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,46 +2,15 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
 
-        int n = nums.size();
+        int n=nums.size();
+        int curMax=0;
+        int  tMax=nums[0];
 
-        // brute force
-        // int maxi = INT_MIN;
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = i; j < n; j++) {
-        //         int sum = 0;
-        //         // add all the elements of subarray:
-        //         for (int k = i; k <= j; k++) {
-        //             sum += arr[k];
-        //         }
-        //         maxi = max(maxi, sum);
-        //     }
-        // }
-        // return maxi;
-
-        // better solution
-
-        // maxi=INT_MIN;
-        // for(int i=0;i<n;i++){
-        //     int sum=0;
-        //     for(int j=i;j<n;j++){
-        //         sum+=arr[j];
-        //         maxi=max(maxi,sum);
-        //     }
-        // }
-        // return maxi;
-
-        // optimal solution
-
-        //..........Kadanes Algorithm..........
-
-         int current_sum = nums[0];
-        int max_sum = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            current_sum = max(nums[i], current_sum + nums[i]);
-            max_sum = max(max_sum, current_sum);
+        for(int i=0;i<n;i++){
+            curMax=max(nums[i],curMax+nums[i]);
+            tMax=max(tMax,curMax);
         }
-        
-        return max_sum;
+        return tMax;
+
     }
 };
