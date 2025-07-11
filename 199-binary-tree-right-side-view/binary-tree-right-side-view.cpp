8d4e396d4fcbe72e1cf6a3a5029eba01076793b4
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-11 12:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (17.99%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -30,43 +30,40 @@ void solve(TreeNode* root,int level,vector<int>&ans){
 
 
 
-        // vector<int>ans;
-        // if(root==NULL)return ans;
+        vector<int>ans;
+        if(root==NULL)return ans;
 
-        // map<int,int>mpp;    //  store{level,level ka last node}
+        map<int,int>mpp;    //  store{level,level ka last node}
 
 
-        // queue<pair<TreeNode*,int>>q;
+        queue<pair<TreeNode*,int>>q;
 
-        // q.push({root,0});
+        q.push({root,0});
 
-        // while(!q.empty()){
+        while(!q.empty()){
             
-        //    pair<TreeNode*,int>p=q.front();
-        //     q.pop();
+           pair<TreeNode*,int>p=q.front();
+            q.pop();
 
-        //     TreeNode* node=p.first;
+            TreeNode* node=p.first;
 
-        //     int level=p.second;
-
-
-
-        //     // if(mpp.find(line)==mpp.end()){
-        //         mpp[level]=node->val;
-        //     // }
-
-        //     if(node->left)q.push({node->left,level+1});
-        //     if(node->right)q.push({node->right,level+1});
-
-
-
-        // }
-
-        // for(auto x:mpp){
+            int level=p.second;
             
-        //     ans.push_back(x.second);
-        // }
-        // return ans; 
+            mpp[level]=node->val;
+            
+
+            if(node->left)q.push({node->left,level+1});
+            if(node->right)q.push({node->right,level+1});
+
+
+
+        }
+
+        for(auto x:mpp){
+            
+            ans.push_back(x.second);
+        }
+        return ans; 
 
 
 
@@ -74,9 +71,9 @@ void solve(TreeNode* root,int level,vector<int>&ans){
         // Approach -2 right boundary traversal
 
 
-        vector<int>ans;
-        if(root==NULL)return ans;
-        solve(root,0,ans);
-        return ans;
+        // vector<int>ans;
+        // if(root==NULL)return ans;
+        // solve(root,0,ans);
+        // return ans;
     }
 };
