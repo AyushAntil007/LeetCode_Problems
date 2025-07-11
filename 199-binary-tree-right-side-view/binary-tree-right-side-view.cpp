a613ac74a75e5bf37389aa2a7103c8a420481dec
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-11 12:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (83.30%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,43 +11,72 @@
  */
 class Solution {
 public:
+
+void solve(TreeNode* root,int level,vector<int>&ans){
+    if(root==NULL)return;
+    
+    if(level==ans.size()){
+        ans.push_back(root->val);
+    }
+    
+
+    solve(root->right,level+1,ans);
+
+    
+    solve(root->left,level+1,ans);
+    
+}
     vector<int> rightSideView(TreeNode* root) {
+
+
+
+        // vector<int>ans;
+        // if(root==NULL)return ans;
+
+        // map<int,int>mpp;    //  store{level,level ka last node}
+
+
+        // queue<pair<TreeNode*,int>>q;
+
+        // q.push({root,0});
+
+        // while(!q.empty()){
+            
+        //    pair<TreeNode*,int>p=q.front();
+        //     q.pop();
+
+        //     TreeNode* node=p.first;
+
+        //     int level=p.second;
+
+
+
+        //     // if(mpp.find(line)==mpp.end()){
+        //         mpp[level]=node->val;
+        //     // }
+
+        //     if(node->left)q.push({node->left,level+1});
+        //     if(node->right)q.push({node->right,level+1});
+
+
+
+        // }
+
+        // for(auto x:mpp){
+            
+        //     ans.push_back(x.second);
+        // }
+        // return ans; 
+
+
+
+
+        // Approach -2 right boundary traversal
+
+
         vector<int>ans;
         if(root==NULL)return ans;
-
-        map<int,int>mpp;    //  store{level,level ka last node}
-
-
-        queue<pair<TreeNode*,int>>q;
-
-        q.push({root,0});
-
-        while(!q.empty()){
-            
-           pair<TreeNode*,int>p=q.front();
-            q.pop();
-
-            TreeNode* node=p.first;
-
-            int level=p.second;
-
-
-
-            // if(mpp.find(line)==mpp.end()){
-                mpp[level]=node->val;
-            // }
-
-            if(node->left)q.push({node->left,level+1});
-            if(node->right)q.push({node->right,level+1});
-
-
-
-        }
-
-        for(auto x:mpp){
-            
-            ans.push_back(x.second);
-        }
-        return ans; 
+        solve(root,0,ans);
+        return ans;
     }
 };
