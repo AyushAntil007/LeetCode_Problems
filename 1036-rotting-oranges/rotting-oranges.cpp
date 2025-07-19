2e66c8da24d168b9e8b459be5fb8dc37ab768d3a
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-19 12:25:53 +0530

    Time: 2 ms (34.75%) | Memory: 17 MB (56.13%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,49 +1,61 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int days=0,tCount=0,count=0;
+void bfs(queue<pair<int,int>>&q,vector<vector<int>>&grid,vector<vector<int>>&vis,int &t,int n,int m){
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    while(!q.empty()){
 
-        int m=grid.size();
-        int n=grid[0].size();
 
-        queue<pair<int,int>>rotten;
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]!=0){
-                   tCount++;
-                }
-                if(grid[i][j]==2){
-                    rotten.push({i,j});
-                }
-            }
-        }
-        int dx[4]={0,+1,0,-1};
-        int dy[4]={+1,0,-1,0};
-        while(!rotten.empty()){
-            
-            int k=rotten.size();
-            count+=k;
-            while(k--){
-                
-                int x=rotten.front().first;
-                int y=rotten.front().second;
-                rotten.pop();
+        int k=q.size();
 
-                for(int i=0;i<4;i++){
-                    int nx=x+dx[i];
-                    int ny=y+dy[i];
+        while(k--){
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
 
-                    if(nx<0||ny<0||nx>=m||ny>=n||grid[nx][ny]!=1)continue;
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+
+                if(nx<n&&nx>=0&& ny<m&& ny>=0&& !vis[nx][ny]&& grid[nx][ny]==1){
+                    vis[nx][ny]=1;
                     grid[nx][ny]=2;
-                    rotten.push({nx,ny});
-                    
+                    q.push({nx,ny});
                 }
             }
-            if(!rotten.empty()){
-                days++;
+
+        }
+        if(!q.empty()){
+            t++;
+        }
+       
+
+
+    }
+
+}
+    int orangesRotting(vector<vector<int>>& grid) {
+
+        int t=0;
+        int n=grid.size();
+        int m=grid[0].size();
+
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2)q.push({i,j});
             }
         }
-        return tCount==count?days:-1;
+
+        bfs(q,grid,vis,t,n,m);
+
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)return -1;
+            }
+        }
+        return t;
     }
 };
