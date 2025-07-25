a2edce1d6ffe42c4bcf770e200ed8affd5b137af
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-25 07:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (9.96%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
+void bfs(int sr,int sc,vector<vector<int>>&image,int color,vector<vector<int>>&vis,queue<pair<int,int>>&q,int n,int m){
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
 
-void bfs(int sr,int sc,vector<vector<int>>&image,vector<vector<int>>&vis,queue<pair<int,int>>&q,int color,int n,int m){
-    
-   int initColor=image[sr][sc];
-
-    vis[sr][sc]=1;
+    int initColor=image[sr][sc];
     image[sr][sc]=color;
     q.push({sr,sc});
 
-    int dr[4]={-1,0,1,0};
-    int dc[4]={0,1,0,-1};
-
     while(!q.empty()){
 
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
 
-        int r=q.front().first;
-        int c=q.front().second;
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
 
-        q.pop();
-
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-            if(nr>=0&&nr<m && nc>=0&&nc<n  && image[nr][nc]==initColor&& !vis[nr][nc]){
-                    vis[nr][nc]=1;
-                    image[nr][nc]=color;
-                    q.push({nr,nc});
+                if(nx<m&&nx>=0&& ny<n&& ny>=0&& !vis[nx][ny]&& image[nx][ny]==initColor){
+                    vis[nx][ny]=1;
+                    image[nx][ny]=color;
+                    q.push({nx,ny});
                 }
-         }
+            }
 
+        }
     }
-        
-}
-    
-
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-        int m=image.size();
+         int m=image.size();
         int n=image[0].size();
 
-        vector<vector<int>>vis(m,vector<int>(n,0));
         queue<pair<int,int>>q;
-
-        bfs(sr,sc,image,vis,q,color,n,m);
-        
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        bfs(sr,sc,image,color,vis,q,n,m);
         return image;
-        
     }
 };
