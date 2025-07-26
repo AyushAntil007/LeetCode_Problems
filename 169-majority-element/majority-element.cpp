d65fe699c78e3c88349a86e9865bc4c30174232a
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-26 10:00:07 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (61.89%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,60 +1,45 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& v) {
-        int n = v.size();
-
-        // brute force
-
-        // for (int i = 0; i < n; i++) {
-        //     int cnt = 0;
-        //     for (int j = 0; j < n; j++) {
-        //         if (v[j] == v[i]) {
-        //             cnt++;     // counting the frequency of v[i]
-        //         }
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     int cnt=0;
+        //     for(int j=i;j<n;j++){
+        //         if(nums[i]==nums[j])cnt++;
         //     }
-        //     if (cnt > (n / 2))    // check if frquency is greater than n/2:
-        //         return v[i];
+        //     if(cnt>n/2)return nums[i];
         // }
         // return -1;
 
-        // better solution
 
-        // map<int, int> mpp;
+        ///hasing
 
-        // for (int i = 0; i < n; i++) {
-        //     mpp[v[i]]++;
+        // unordered_map<int,int>mpp;
+        // for(int i=0;i<n;i++){
+        //     mpp[nums[i]]++;
         // }
-        // for (auto it : mpp) {
-        //     if (it.second > (n / 2)) {
-        //         return it.first;
-        //     }
+
+        // for(auto it: mpp){
+        //     if(it.second>n/2)return it.first;
         // }
         // return -1;
 
-        // optimal solution
+        //optimal 
 
-        // Moore’s Voting Algorithm:
+        int elt=nums[0];
+        int cnt=1;
 
-        int elt = v[0];
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (v[i] == elt)
-                cnt++;   //frequency of elt
-            else
-                cnt--;   //cancelling the non mazority elts with mazority elt
-
-            if (cnt == 0) {
-                elt = v[i + 1];
+        for(int i=1;i<n;i++){
+            if(nums[i]==elt)cnt++;
+            else{
+                cnt--;
+                if(cnt==0){
+                    elt=nums[i];
+                    cnt=1;
+                }
             }
         }
-        cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == elt)   
-                cnt++;
-        }
-        if (cnt > n / 2)   //checking valid mazor elt or not
-            return elt;
-        return -1;
+        return elt;
     }
+    
 };
