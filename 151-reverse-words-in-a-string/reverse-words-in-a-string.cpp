74de574c6c22d72f2f570f6199ee0c242e1d9d69
AyushAntil007 <ayushantil2023@gmail.com>
2025-07-26 18:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.14%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,54 +1,35 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.length();
-       string ans="";
-        stack<string> st;
+        if(s.empty())return s;
 
-        // my approach....using set
-        //  int start = 0;
-        //  int end = 0;
-        //  int i = 0;
+        int n=s.length();
+        string ans="";
+        int start=n-1,end=n-1;
+       int i=n-1;
 
-        // while (i < n) {
-        //     while (i < n && s[i] == ' ') {
-        //         i++;
-        //     }
-        //     start = i;
-        //     while (i < n && s[i] != ' ') {
-        //         i++;
-        //     }
-        //     end = i;
-        //     if (start < end) { // Ensure a word was found
-        //         st.push(s.substr(start, end - start));
-        //     }
-        // }
+        while(i>=0){
+            while(i>=0&&s[i]==' ' )i--;
 
-        //  while (!st.empty()) {
-        //     ans += st.top();
-        //     st.pop();
-        //     if (!st.empty()) ans += " "; // Add space between words
-        // }
+    
 
-//optimal sol.....traverse from back
-         int start = n-1;
-         int end = n-1;
-         int i = n-1;
 
-        while (i >=0) {
-            while (i >=0 && s[i] == ' ') {
+            end=i;
+            while( i>=0&&s[i]!=' '){
                 i--;
+                
             }
-            end = i;
-            while (i >=0 && s[i] != ' ') {
-                i--;
-            }
-            start = i;
-            if (start < end) { // Ensure a word was found
-               ans+=s.substr(start+1,end-start)+' ';
-            }
+            start=i+1;
+            
+           if(start<=end) {
+            ans+=s.substr(start,end-start+1)+" ";
+           }
         }
-        if (!ans.empty()) ans.pop_back(); // Remove trailing space
+           
+        
+        if(!ans.empty())ans.pop_back();
+
         return ans;
+        
     }
 };
