d7a42ddd7843991d59667388e9ed5471b4e264db
AyushAntil007 <ayushantil2023@gmail.com>
2025-08-02 17:30:56 +0530

    Time: 32 ms (81.41%) | Memory: 75.1 MB (57.00%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -27,7 +27,7 @@ public:
 
         while(r < n){
             freq[fruits[r]]++;
-            while(freq.size()>2){
+            if(freq.size()>2){
                   freq[fruits[l]]--;
                   if(freq[fruits[l]]==0){
                     freq.erase(fruits[l]);
