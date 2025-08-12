899295039561ad89f6bfc1f36eb49e3f2fba9915
AyushAntil007 <ayushantil2023@gmail.com>
2025-08-12 21:53:32 +0530

    Time: 1251 ms (7.49%) | Memory: 295.6 MB (6.54%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,74 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> st;
-        vector<vector<int>> ans;
-        int n = nums.size();
+        
 
-        // brute force..............3 for loop
-        //     for(int i=0;i<n;i++){
-        //         for(int j=i+1;j<n;j++){
-        //             for(int k=j+1;k<n;k++){
-        //                  if(nums[i]+nums[j]+nums[k]==0){
-        //                       vector<int>subAns={nums[i],nums[j],nums[k]};
-        //                       sort(subAns.begin(),subAns.end());   //sort the
-        //                       pairs st.insert(subAns);
-        //                  }
-        //             }
-        //         }
-        //     }
+        // 3 pointers
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
 
-        // better sol......check in set
+        vector<vector<int>>ans;
+        set<vector<int>>st;
 
-        // for (int i = 0; i < n - 2; i++) {
-        //      set<int> thirdElt;
-        //     for (int j = i + 1; j < n ; j++) {
-        //         int third = 0 - nums[i] - nums[j];
+        for(int i=0; i<n-2; i++){
+            int left=i+1,right=n-1;
+            int a=nums[i];
 
-        //         if (thirdElt.find(third) != thirdElt.end()) {
-        //             vector<int> subAns = {nums[i], nums[j], third};
-        //             sort(subAns.begin(), subAns.end());
-        //             st.insert(subAns);
-        //         }
+            while(left<right){
+                int b=nums[left],c=nums[right];
 
-        //          thirdElt.insert(nums[j]);
-        //     }
-        // }
-
-        // optimal solution........sorting and 2 pointer
-
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < n; i++) {
-            int left = i + 1, right = n - 1;
-
-            if (i > 0 && nums[i] == nums[i - 1])   // to skip duplkicates
-                continue;
-
-            while (left < right) {   // two pointer approach
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum < 0) {
-                    left++;
-                } else if (sum > 0) {
-                    right--;
-                } else {
-                    vector<int> temp = {nums[i], nums[left], nums[right]};
-                    ans.push_back(temp);
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left -1]) {   //skip duplicates
-                        left++;
-                    }
-                    while (right > left && nums[right] == nums[right + 1]) {   //skip duplivcates
-                        right--;
-                    }
+                if(a+b+c==0){
+                    st.insert({a,b,c});
+                    left++,right--;
                 }
+                else if(a+b+c<0)left++;
+                else right--;
             }
         }
 
-        // for (auto it : st) { // store set elts in ans
-        //     ans.push_back(it);
-        // }
+        
+        for(auto it : st)ans.push_back(it);
+        
         return ans;
     }
 };
