b2782cd70ccaf2fd768dfe210a4c601b0e61fce5
AyushAntil007 <ayushantil2023@gmail.com>
2025-08-13 22:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (94.61%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,50 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-
-    bool isPossible(TreeNode* root, long long l, long long r) {
-        if (root == nullptr)
-            return true;
-        if (root->val < r and root->val > l)   //track the range min< root->val  <max
-            return isPossible(root->left, l, root->val) &&
-                   isPossible(root->right, root->val, r);
-        else
-            return false;
-    }
-    void isPossible(TreeNode* root, bool& flag) {
-        if (root == nullptr)
-            return;
-        if (root->left != NULL && root->val <= root->left->val) {   //check root->left and root->right with root
-
-            flag = false;
-            return;
-        }
-        if (root->right != NULL && root->val >= root->right->val) {
-            flag = false;
-            return;
-        }
-
-        isPossible(root->left, flag);
-        isPossible(root->right, flag);
-    }
-
 public:
+
+bool solve(TreeNode*root, long long maxi, long long mini){
+    if(root==NULL)return true;
+
+    if(root->val >= maxi || root->val<=mini){
+        return false;
+    
+    }
+    bool left=solve(root->left, root->val,mini);
+    bool right=solve(root->right,maxi,root->val);
+
+        return left&&right;
+}
     bool isValidBST(TreeNode* root) {
 
-        //........Approach-1...........few test cases are not passed
+       long long int mini = -1000000000000, maxi = 1000000000000;
 
-        // bool flag = true;
-        // isPossible(root, flag);
-        // return flag;
+        return solve(root,maxi,mini);
 
-        //........Approach-2.......... this is the better approach for this question
-
-        long long int min = -1000000000000, max = 1000000000000;
-        return isPossible(root, min, max);
+        
     }
 };
