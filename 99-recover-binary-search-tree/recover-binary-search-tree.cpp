94f5eec7adee23f345b18e6c77c45aba34702fb1
AyushAntil007 <ayushantil2023@gmail.com>
2025-08-27 06:54:19 +0530

    Time: 2 ms (23.39%) | Memory: 60.9 MB (30.87%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -12,25 +12,41 @@
 class Solution {
 public:
 
-void inord(TreeNode* root, vector<int>&elts, bool check,int &idx){
+void solve(TreeNode*root, TreeNode* &prev, TreeNode*&first, TreeNode*&mid, TreeNode*&last){
     if(root==NULL)return;
 
-    inord(root->left,elts,check,idx);
-    if(check)root->val=elts[idx++];
-   else  elts.push_back(root->val);
-    inord(root->right,elts,check, idx);
+    solve(root->left,prev,first,mid,last);
+
+    if(prev &&prev->val>root->val){
+        if(!first){
+            first=prev;
+            mid=root;
+        }
+        else{
+            last=root;
+        }
+
+    }
+    prev=root;
+
+    solve(root->right,prev,first,mid,last);
 }
     void recoverTree(TreeNode* root) {
-        vector<int>elts;
-        bool check=false;
-         int idx=0;
-
-
-        inord(root,elts,check,idx);
-        sort(elts.begin(),elts.end());
-        check=true;
-       
         
-        inord(root,elts,check,idx);
+
+        //swap node are adjacent
+
+        //swap nodes aree not adjacent
+
+        TreeNode*prev=new TreeNode(INT_MIN);
+        TreeNode*first=NULL;
+        TreeNode*mid=NULL;
+        TreeNode*last=NULL;
+
+        solve(root, prev,first,mid,last);
+
+        if(first&&last)swap(first->val,last->val);
+        else if(first&&mid)swap(first->val,mid->val);
+        
     }
 };
