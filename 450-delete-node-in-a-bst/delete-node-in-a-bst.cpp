3e31e6a1df0fccd32c82cb8d414ebc5b325e5ca7
AyushAntil007 <ayushantil2023@gmail.com>
2025-08-27 18:57:18 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (30.74%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -11,35 +11,32 @@
  */
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root==NULL)return root;
 
-        if(root->val==key){  // if key is present 
-            if(root->left==NULL&& root->right==NULL) return NULL;   //case -1  key is leaf node
-               
-            else if(root->left==NULL){    // key is having single child
-                return root->right;
-            }
-            else if(root->right==NULL){ 
-                return root->left;
-            }
-            else{     //key is having two child
-                
-                TreeNode* temp=root->left;
-                while(temp->right){
-                    temp=temp->right;
-                }
-                root->val=temp->val;
-                root->left = deleteNode(root->left, temp->val); 
+int pred(TreeNode* root){
+    while(root->right)root=root->right;
+    return root->val;
+}
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL)return NULL;
+        if(root->val==key){
+            
+            if(!root->left && !root->right)return NULL;
+            else if(!root->right)return root->left;
+            else if(!root->left)return root->right;
+            else {
+                int elt=pred(root->left);
+                root->val=elt;
+                root->left=deleteNode(root->left,elt);
                 return root;
             }
+
         }
-        else if(key>root->val){       
-            root->right=deleteNode(root->right,key);
-        }
-        else {
+        if(key<root->val){
             root->left=deleteNode(root->left,key);
+            
         }
+        else root->right=deleteNode(root->right,key);
+
         return root;
     }
 };
