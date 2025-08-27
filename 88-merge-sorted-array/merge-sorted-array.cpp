52dd7ab5b71f7406381e2125b95ba8144c8023dc
AyushAntil007 <ayushantil2023@gmail.com>
2025-08-27 09:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.79%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,65 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-
-        // brute force......put nums2 in nums1 and sort
-        //  for(int i=m;i<m+n;i++){
-        //      nums1[i]=nums2[i-m];
-        //  }
-        //  sort(nums1.begin(),nums1.end());
-
-        // beter solution....using extra array
-
-        //     vector<int>dup(m+n);
-        //     int i=0,j=0;
-        //     int idx=0;
-        //     while(i<n&&j<m){
-        //         if(nums1[i]<nums2[j]){
-        //             dup[idx]=(nums1[i]);
-        //             i++,idx++;
-        //         }
-        //         else{
-        //             dup[idx]=(nums2[j]);
-        //             j++,idx++;
-        //         }
-        //     }
-        //     while(i<m){
-        //         dup[idx]=(nums1[i]);
-        //         i++,idx++;
-        //     }
-        //     while(j<n){
-        //          dup[idx]=(nums2[j]);
-        //         j++,idx++;
-        //     }
-
-        //    for(int i=0;i<m+n;i++){
-        //     nums1[i]=dup[i];
-        //    }
-
-        // better sol
-
-        int i = m-1, j = 0;
-        while (i>=0 && j < n) {
-            if (nums1[i] > nums2[j]) {
-                swap(nums1[i],nums2[j]);
-                i--,j++;
-            }
-            else{
-                break;
-            }
-        }
-        sort(nums1.begin(), nums1.begin() + m); // Sort the valid part of nums1
-        sort(nums2.begin(), nums2.end());
-
-        for (int i = m; i < m + n; i++) {
-            nums1[i] = nums2[i - m];
-         }
-
-        // gap method or   shell sort
-
-
         
 
+        int idx=m+n-1;
 
+        int i=m-1,j=n-1;          //  bigger elts compare......
+
+        while(i>=0&&j>=0){
+            if(nums1[i]>=nums2[j])nums1[idx--]=nums1[i--];
+            else nums1[idx--]=nums2[j--];
+        }
+
+        while(i>=0){
+            nums1[idx--]=nums1[i--];
+        }
+        while(j>=0){
+            nums1[idx--]=nums2[j--];
+        }
+
+        
     }
 };
