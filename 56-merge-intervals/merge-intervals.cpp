cdd54fdd221ce6e84041787eba6111702a179f54
AyushAntil007 <ayushantil2023@gmail.com>
2025-09-01 07:45:00 +0530

    Time: 3 ms (89.51%) | Memory: 23.9 MB (57.19%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& nums) {
-        int n=nums.size();
-        vector<vector<int>>ans;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
 
-        sort(nums.begin(),nums.end());
-        int end=nums[0][1];
-        int start=nums[0][0];
+        sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>>ans;
         
+        int start=intervals[0][0];
+        int end=intervals[0][1];
         for(int i=1;i<n;i++){
-            
-            if(nums[i][0]<=end ){
-                end=max(end,nums[i][1]);   // max.....kya pata pura interval hi merge ho jay
-            }                              //update end when merge interval exist
-            else{
-                ans.push_back({start,end});  //store previous interval
-                start=nums[i][0];
-                end=nums[i][1];
+          
+
+            if(intervals[i][0]<=end){
+                end=max(end,intervals[i][1]);
                 
-            } 
+            }
+            else{
+                
+                
+                ans.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+
+            }
+           
         }
-        ans.push_back({start,end});    //store last interval
+
+        ans.push_back({start,end});
+
+
         return ans;
     }
 };
