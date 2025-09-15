098ba57d17392aac5e006c75d397c9ee033c37d1
AyushAntil007 <ayushantil2023@gmail.com>
2025-09-15 18:45:27 +0530

    Time: 4 ms (56.60%) | Memory: 14.8 MB (36.11%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& arr, int target) {
-        int n = arr.size();
+    vector<int> twoSum(vector<int>& nums, int target) {
 
-        // brute force
 
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = i + 1; j < n; j++) {
-        //         if (arr[i] + arr[j] == target) {
-        //             return {i, j};
-        //         }
-        //     }
-        // }
-        // return {-1, -1};
+        //brute force
 
-        // better solution
 
-        unordered_map<int, int> mpp;
-        for (int i = 0; i < n; i++) {
-            int num = arr[i];
-            int moreNeeded = target - num;
-            if (mpp.find(moreNeeded) != mpp.end()) {
-                return {mpp[moreNeeded], i};
+        //sort(nums.begin(),nums.end());
+
+        unordered_map<int,int>prefix;
+        int n=nums.size();
+
+        for(int i=0; i<n; i++){
+
+            int a=nums[i];
+            int b=target-a;
+
+
+            if(prefix.find(b)!=prefix.end()){
+                return { i, prefix[b]};
             }
-            mpp[num] = i;
+
+            prefix[a]=i;
+
         }
-        return {-1, -1};
-
-        // optimal solution..............only provide yes or no*****
-
-        // sort(arr.begin(), arr.end());
-        // int left = 0, right = n - 1;
-        // while (left < right) {
-        //     int sum = arr[left] + arr[right];
-        //     if (sum == target) {
-        //         return "yes";
-        //     }
-        //     else if (sum < target)
-        //         left++;
-        //     else
-        //         right--;
-        // }
-        // return "no";
+        return {-1,-1};
+        
     }
 };
