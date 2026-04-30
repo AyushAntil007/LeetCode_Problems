e984330f8d7f4cd2150b9307a85890b7e4d02efe
AyushAntil007 <ayushantil2023@gmail.com>
2026-04-30 17:53:05 +0530

    Time: 43 ms (74.61%) | Memory: 91 MB (24.51%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,13 +4,23 @@ public:
 
         int n = nums.size();
 
-        unordered_map<int,int> freq;
+        // unordered_map<int,int> freq;
 
-        for(int i=0 ; i < n; i++) {
-            freq[nums[i]]++;
-            if(freq[nums[i]] > 1)return true;
+        // for(int i=0 ; i < n; i++) {
+        //     freq[nums[i]]++;
+        //     if(freq[nums[i]] > 1)return true;  
+        // }
 
-            
+
+        //using set
+
+        unordered_set<int> st;
+
+        for(int i=0; i<n; i++){
+            if(st.find(nums[i]) != st.end()){
+                return true;
+            }
+            st.insert(nums[i]);
         }
 
         return false;
