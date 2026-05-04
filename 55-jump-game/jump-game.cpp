e46b946d96aa44ab2953b0d2a79f0f36b9812cf3
AyushAntil007 <ayushantil2023@gmail.com>
2026-05-04 18:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (98.21%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+
+        int maxStep=0;
+
         int n = nums.size();
-        int maxIdx = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (maxIdx < i) {  // 0 jump par hi fas gya in all possible ways
-                return false;
-            }
-            maxIdx = max(maxIdx, i + nums[i]);
+        for(int i=0; i<n; i++){
 
-            if (maxIdx >= n - 1) //last elt par ho gya
-                return true;
+            if(nums[i]==0 && maxStep == i && n>1)return false;
+
+            maxStep = max( maxStep, i+nums[i]);
+
+            if( maxStep>= n-1)return true;
         }
-        return true;
+
+        return false;
+        
     }
 };
