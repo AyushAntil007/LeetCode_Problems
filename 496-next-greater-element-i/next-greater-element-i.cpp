1cfd053261ad0c88bf7914a538615f087d5ec69e
AyushAntil007 <ayushantil2023@gmail.com>
2026-05-09 09:28:35 +0530

    Time: 26 ms (5.60%) | Memory: 12.8 MB (59.21%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n2 = nums2.size();
+
+        int  n2 = nums2.size();
 
         stack<int>st;
 
-        unordered_map<int,int> mpp;
+        unordered_map<int,int>mpp;
 
-        for(int i= n2-1; i>=0; i--) {
+        for(int i=n2-1; i>=0; i--) {
 
+            while(!st.empty() && nums2[i] > st.top())st.pop();
+            
             
 
-            while(! st.empty() && st.top()< nums2[i]) st.pop();
+            if(!st.empty() && nums2[i]< st.top())mpp[nums2[i]] = st.top();
 
-            if(st.empty())mpp[nums2[i]] = -1;
-
-            else if(nums2[i] < st.top())mpp[nums2[i]]=st.top();
+            if(st.empty())mpp[nums2[i]]=-1;
 
             st.push(nums2[i]);
+        }
+
+        for(auto &it : mpp) {
+            cout<<it.first <<" "<<it.second<<endl;
+        }
+
+      vector<int>ans;
+        
+
+        for(int i=0; i<nums1.size(); i++) {
+            if(mpp[nums1[i]])ans.push_back(mpp[nums1[i]]);
+            
 
         }
 
-        vector<int>ans;
-
-        for(int i=0; i<nums1.size(); i++) {
-            if(mpp.count(nums1[i])) ans.push_back(mpp[nums1[i]]);
-
-            else ans.push_back(-1);
-    }
-
-    return ans;
-            
+        return ans;
     }
 };
