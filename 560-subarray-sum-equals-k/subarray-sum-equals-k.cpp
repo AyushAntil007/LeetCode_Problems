a5197b7e7884242070c7012cb3524c204332bbc1
AyushAntil007 <ayushantil2023@gmail.com>
2026-05-09 11:33:50 +0530

    Time: 35 ms (86.10%) | Memory: 45.5 MB (38.23%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        int count=0;
-        int n=arr.size();
-
-        //Approach-1....using for loops
-
-        // for(int i=0;i<n;i++){
-        //     int sum=0;
-        //    
-        //     for(int j=i+1;j<n;j++){
-        //         sum+=arr[j];
-        //         if(sum==k){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // return count;
-
-
-        //Approach-2....using mapping
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
 
         unordered_map<int,int>mpp;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
-            if(sum==k)count++;
 
-            int rem=sum-k;
-            if(mpp.find(rem)!=mpp.end()){
-                count+=mpp[rem];
+        int prefSum = 0;
+        int cnt = 0;
+
+        for(int i=0; i<n; i++) {
+
+            prefSum += nums[i];
+
+            if(prefSum== k) cnt++;
+
+            int leftover = prefSum - k;
+
+            if(!mpp.empty() && mpp.find(leftover) != mpp.end()) {
+
+                cnt += mpp[leftover];
+                
             }
-            mpp[sum]++;
+
+            mpp[prefSum] ++;
+
+
         }
-        return count;
+
+        return cnt;
+
+          
+        
     }
 };
